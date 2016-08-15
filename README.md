da4447bc75195cac5dd9d1948fa9000e277ccbe1
Dominik Dario <dominik@doda.co>
2016-08-14 23:00:42 -0400

    Build README manually

---
+++ README.md
@@ -0,0 +1,11 @@
+A JSON object mapping which colors github uses to represent different programming languages in their "Languages" tab on each repository.
+
+```
+{
+    "Mercury": "#ff2b2b", 
+    "TypeScript": "#2b7489",
+    "PureBasic": "#5a6986",
+    ...
+```
+
+![](/logo.png?raw=true)
--- build.py
+++ build.py
@@ -17,9 +17,3 @@ colors = dict((replace_names.get(name, name), color['color']) for name, color in
 
 with open('colors.json', 'w') as f:
     json.dump(colors, f, indent=4)
-
-with open('README.rst', 'w') as f:
-    f.write('A mapping of which colors github uses to represent different programming languages in their "Languages" tab on each repository.')
-    f.write('\n\n')
-    color_strings = ('.. image:: http://www.placehold.it/150/%s/ffffff&text=%s' % (v[1:].lower(), k) for k, v in colors.items())
-    f.write('\n'.join(sorted(color_strings)))
