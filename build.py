16e3dc376f4cb79c3e81cbf7cdae27aa6e6b783c
Charles Flèche <charles.fleche@free.fr>
2018-04-04 11:54:47 +0200

    Use requests in place of wget for a pure python implementation

--- build.py
+++ build.py
@@ -1,18 +1,18 @@
-import subprocess
 import yaml
 import json
 import os
+import requests
+
+languages_url = 'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml'
 
 replace_names = {
     'C++': 'cpp',
     'C#': 'C Sharp'
 }
 
-subprocess.call(['wget', 'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml', '-q'])
 
-with open('languages.yml') as f:
-    colors = yaml.load(f)
-os.remove('languages.yml')
+r =  requests.get(languages_url)
+colors = yaml.load(r.text)
 colors = dict((replace_names.get(name, name), color['color']) for name, color in colors.items() if 'color' in color)
 
 with open('colors.json', 'w') as f:
